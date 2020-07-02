d5c6f879ef4d5586a6c29c19c82010839bfc6d43
Rohmad <rohmad37@ymail.com>
2020-07-02 11:36:22 +0700

    Revisi Fungsi di Pisahin

--- src/components/Counter/Function/Counter2.js
+++ src/components/Counter/Function/Counter2.js
@@ -2,23 +2,27 @@ import React, { useState } from "react";
 
 function Counter2() {
   const [count, setCount] = useState(0);
-  const [reset] = useState(0);
+
+  const Plus = () => {
+    setCount(count + 1);
+  };
+  const Min = () => {
+    if (count < 1) {
+      alert("Angka Tidak Boleh 0");
+    } else {
+      setCount(count - 1);
+    }
+  };
+  const Reset = () => {
+    setCount(0);
+  };
+
   return (
     <div>
       <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}> + </button>
-      <button
-        onClick={() => {
-          if (count < 1) {
-            alert("Angka Tidak Boleh 0");
-          } else {
-            setCount(count - 1);
-          }
-        }}
-      >
-        -
-      </button>
-      <button onClick={() => setCount(0)}> Reset </button>
+      <button onClick={Plus}> + </button>
+      <button onClick={Min}>-</button>
+      <button onClick={Reset}> Reset </button>
     </div>
   );
 }
